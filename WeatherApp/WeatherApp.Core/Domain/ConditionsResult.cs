796a246db6d1ab9021c4078de928714fc746b4f9
Chris Duflo <chris.duflo@gmail.com>
2016-01-26 12:04:15 -0800

    core functionality completed, next are extras and beautification

---
+++ WeatherApp/WeatherApp.Core/Domain/ConditionsResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class ConditionsResult
+    {
+        // public string display_location { get; set; }
+        // public string display_location.full { get; set; }
+        // public string display_location.latitude { get; set; }
+        //  public string display_location.longit { get; set; }
+        //  public float display_location.elevation { get; set; }
+        public string full { get; set; }
+        public string latitude { get; set; }
+         public string longitude { get; set; }
+         public string elevation { get; set; }
+        public string weather { get; set; }
+        public string temperature_string { get; set; }
+        public string feelslike_string { get; set; }
+        public string wind_mph { get; set; }
+        public string wind_dir { get; set; }
+        public string relative_humidity { get; set; }
+        public string visibility_mi { get; set; }
+        public string UV { get; set; }
+        public string precip_today_string { get; set; }
+        public string icon { get; set; }
+        public string icon_url { get; set; }
+        public string observation_time { get; set; }
+    }
+}
---
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WeatherApp.Core.Services;
+using WeatherApp.Core;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonQuery_Click(object sender, RoutedEventArgs e)
+        {
+            string query = textBoxQuery.Text;
+            ConditionsResult result = WeatherService.GetWeatherFor(query);
+            labelLocation.Content = result.full;
+            labelTempF.Content = "Temperature: "+result.temperature_string;
+            labelTempC.Content = "Feels Like: "+result.feelslike_string;
+            labelElev.Content = "Elevation: "+result.elevation;
+            labelLatLong.Content = "Latitude / Longitude: " + result.latitude + "/" + result.longitude;
+            labelWindDir.Content = "Wind Direction: "+result.wind_dir;
+            labelConditions.Content = result.weather;
+            labelUpdate.Content = result.observation_time;
+            labelHumid.Content = "Humidity: "+result.relative_humidity;
+            labelVis.Content = "Visibility: "+result.visibility_mi+" miles";
+            labelUV.Content = "UV: "+result.UV;
+            labelPrec.Content = "Precipitation: "+result.precip_today_string;
+            labelWind.Content = "Wind: " + result.wind_mph;
+            imageWeather.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(result.icon + ".gif");
+        }
+    }
+}
